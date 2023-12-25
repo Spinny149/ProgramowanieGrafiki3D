9fe1270f6ae87c082b5ad79fe6cdce2dcc106ba3
Spinny149 <czechulok@gmail.com>
2023-12-25 14:51:05 +0100

    CameraMovement done

---
+++ src/Assignments/CameraMovement/app.h
@@ -0,0 +1,57 @@
+//
+// Created by pbialas on 05.08.2020.
+//
+
+#pragma once
+
+#include <vector>
+#include <glm/glm.hpp>
+
+#include "Application/application.h"
+#include "Application/utils.h"
+#include "glad/gl.h"
+
+#include "camera.h"
+#include "camera_controler.h"
+
+class SimpleShapeApplication : public xe::Application
+{
+public:
+    SimpleShapeApplication(int width, int height, std::string title, bool debug) : Application(width, height, title, debug) {}
+
+    void init() override;
+
+    void frame() override;
+
+    void framebuffer_resize_callback(int w, int h) override;
+
+    void set_camera(Camera* camera) { camera_ = camera; }
+    Camera* camera() { return camera_; }
+    ~SimpleShapeApplication() {
+        if (camera_) {
+            delete camera_;
+        }
+    }
+
+    void scroll_callback(double xoffset, double yoffset) override;
+
+    void set_controler(CameraControler* controler) { controler_ = controler; }
+
+    void cursor_position_callback(double x, double y);
+
+    void mouse_button_callback(int button, int action, int mods);
+
+private:
+    GLuint vao_;
+
+    float fov_;
+    float aspect_;
+    float near_;
+    float far_;
+
+    CameraControler* controler_;
+
+    GLuint u_pvm_buffer_;
+
+    Camera* camera_;
+};
